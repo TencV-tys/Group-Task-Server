9132f220770dfc1bf512f4049d4c461e29581e8a
Vincent <vincenttayros078@gmail.com>
2026-01-23 20:41:28 +0800

    added user services

--- src/services/user.auth.services.ts
+++ src/services/user.auth.services.ts
@@ -1,12 +1,20 @@
 
 import { UserRole, UserRoleStatus } from "@prisma/client";
 import prisma from "../prisma";
-import { UserAuthTypes } from "../types/user.auth";
+import { UserSignUpAuthTypes, UserLoginAuthTypes } from "../types/user.auth";
 export class UserServices{
 
- static async signup(email:string,name:string,password:string,avatarUrl?:string | null,phone?:string | null):Promise<UserAuthTypes>{
+ static async signup(email:string,name:string,password:string,avatarUrl?:string | null,phone?:string | null):Promise<UserSignUpAuthTypes>{
             try{
-            
+               
+                if(!email || !password || !name ){
+                    return{
+                        success:false,
+                        message:"All fields are required"
+                    }
+                }
+
+
                 const user = await prisma.user.create({
                     data:{
                          name:name,
@@ -49,5 +57,51 @@ export class UserServices{
  }
 
 
+
+ static async login(email:string,password:string):Promise<UserLoginAuthTypes>{
+     try{
+            
+            if(!email || !password){
+                return{
+                    success:false,
+                    message:"All fields are required"
+                }
+            }
+
+        const user = await prisma.user.findUnique({
+            where:{email}
+        });
+
+        if(!user){
+            return{
+                success:false,
+                message:"User not found"
+            }
+        } 
+        
+        const userId = user.id as unknown as string;
+          return{
+             success:true,
+             message:"Login Successfully",
+             user:{
+                id:userId,
+                name:user.name,
+                email:user.email,
+                avatarUrl:user.avatarUrl,
+                phone:user.phone,
+                role:user.role,
+                roleStatus:user.roleStatus
+             }
+          }
+
+     }catch(e:any){
+        return{
+            success:false,
+            message:"Login Failed",
+            error:e.message
+        }
+     }  
+
+ }
 
 }
--- src/types/user.auth.ts
+++ src/types/user.auth.ts
@@ -1,6 +1,6 @@
 import { UserRole, UserRoleStatus } from "@prisma/client";
 
-export interface UserAuthTypes{
+export interface UserSignUpAuthTypes{
 success:boolean;
 message:string;
 token?:string;
@@ -9,6 +9,26 @@ user?:{
     name:string;
     email:string;
     passwordHash:string;
+    avatarUrl?:string | null;
+    phone?:string | null;
+    role:UserRole;
+    roleStatus:UserRoleStatus;
+};
+error?:string;
+
+
+
+}
+
+
+export interface UserLoginAuthTypes{
+success:boolean;
+message:string;
+token?:string;
+user?:{
+    id:string;
+    name:string;
+    email:string;
     avatarUrl?:string | null;
     phone?:string | null;
     role:UserRole;
