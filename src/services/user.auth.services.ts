0f6ac11ac917469e250c1a445544b3ed3f27b075
Vincent <vincenttayros078@gmail.com>
2026-01-23 20:27:35 +0800

    added fixed sign up

--- src/services/user.auth.services.ts
+++ src/services/user.auth.services.ts
@@ -7,30 +7,31 @@ export class UserServices{
  static async signup(email:string,name:string,password:string,avatarUrl?:string | null,phone?:string | null):Promise<UserAuthTypes>{
             try{
             
-                 const userData:any = {
-                        name:name,
+                const user = await prisma.user.create({
+                    data:{
+                         name:name,
                         email:email,
                         passwordHash:password,
                         avatarUrl:avatarUrl ?? null,
                         phone:phone ?? null,
                         role:UserRole.USER ,
                         roleStatus:UserRoleStatus.ACTIVE
+                    
                     }
-
-                const user = await prisma.user.create({
-                    data:userData
                 });
+                
+                const userId = user.id as unknown as string;
 
                  return{
                     success:true,
                     message:"Sign up successfully",
                     user:{
-                        id:user.id as string,
+                        id:userId,
                         name:user.name,
                         email:user.email,
                         passwordHash:user.passwordHash,
-                        avatarUrl:user.avatarUrl ?? undefined,
-                        phone:user.phone ?? '',
+                        avatarUrl:user.avatarUrl,
+                        phone:user.phone,
                         role: user.role,
                         roleStatus: user.roleStatus
                     }
