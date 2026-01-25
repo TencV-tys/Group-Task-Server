2c35316546a318b73a1aaa9ec7a4e4f7129f7bc3
Vincent <vincenttayros078@gmail.com>
2026-01-25 12:37:23 +0800

    added

--- src/routes/admin.auth.routes.ts
+++ src/routes/admin.auth.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { AdminAuthController } from "../controllers/admin.auth.controller";
+import { AdminAuthMiddleware } from "../middlewares/admin.auth.middleware";
 const router = Router();
 
+
 router.post('/login',AdminAuthController.login);
-router.post('/refresh-token', AdminAuthController.refreshToken);
-router.post('/logout', AdminAuthController.logout);
+router.post('/refresh-token',AdminAuthMiddleware, AdminAuthController.refreshToken);
+router.post('/logout',AdminAuthMiddleware, AdminAuthController.logout);
 
 export default router;
 
