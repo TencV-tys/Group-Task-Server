d57109d6bd2725ec6ecca3feff667b449af400b2
Vincent <vincenttayros078@gmail.com>
2026-01-28 19:21:11 +0800

    added routes

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -7,5 +7,6 @@ const router = Router();
 router.use(UserAuthMiddleware);
 
 router.post('/create',GroupController.createGroup);
+router.post('/join',GroupController.joinGroup);
 
 export default router;
